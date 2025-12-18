925a0f81d81d08ba9280777e925e043bdcdf609b
AhmedHero55 <sepertiten@gmail.com>
2025-12-18 01:07:38 +0100

    Update ProChan.kt

--- extension/eu/kanade/tachiyomi/extension/ar/prochan/ProChan.kt
+++ extension/eu/kanade/tachiyomi/extension/ar/prochan/ProChan.kt
@@ -13,27 +13,22 @@ class ProChan : ParsedHttpSource() {
     override val lang = "ar"
     override val supportsLatest = true
 
-    /* ===== Popular ===== */
-    override fun popularMangaRequest(page: Int): Request {
-        return GET("$baseUrl", headers)
-    }
+    override fun popularMangaRequest(page: Int): Request =
+        GET(baseUrl, headers)
 
     override fun popularMangaSelector() = "div.post"
 
-    override fun popularMangaFromElement(element: Element): SManga {
-        return SManga.create().apply {
+    override fun popularMangaFromElement(element: Element): SManga =
+        SManga.create().apply {
             title = element.select("h2 a").text()
             url = element.select("h2 a").attr("href")
             thumbnail_url = element.select("img").attr("src")
         }
-    }
 
     override fun popularMangaNextPageSelector() = null
 
-    /* ===== Latest ===== */
-    override fun latestUpdatesRequest(page: Int): Request {
-        return GET("$baseUrl", headers)
-    }
+    override fun latestUpdatesRequest(page: Int): Request =
+        GET(baseUrl, headers)
 
     override fun latestUpdatesSelector() = popularMangaSelector()
     override fun latestUpdatesFromElement(element: Element) =
@@ -41,14 +36,12 @@ class ProChan : ParsedHttpSource() {
 
     override fun latestUpdatesNextPageSelector() = null
 
-    /* ===== Search ===== */
     override fun searchMangaRequest(
         page: Int,
         query: String,
         filters: FilterList
-    ): Request {
-        return GET("$baseUrl/?s=$query", headers)
-    }
+    ): Request =
+        GET("$baseUrl/?s=$query", headers)
 
     override fun searchMangaSelector() = popularMangaSelector()
     override fun searchMangaFromElement(element: Element) =
@@ -56,30 +49,24 @@ class ProChan : ParsedHttpSource() {
 
     override fun searchMangaNextPageSelector() = null
 
-    /* ===== Details ===== */
-    override fun mangaDetailsParse(document: Document): SManga {
-        return SManga.create().apply {
-            title = document.selectFirst("h1")?.text() ?: ""
+    override fun mangaDetailsParse(document: Document): SManga =
+        SManga.create().apply {
+            title = document.selectFirst("h1")?.text().orEmpty()
             description = document.select("div.description").text()
         }
-    }
 
-    /* ===== Chapters ===== */
     override fun chapterListSelector() = "li.chapter a"
 
-    override fun chapterFromElement(element: Element): SChapter {
-        return SChapter.create().apply {
+    override fun chapterFromElement(element: Element): SChapter =
+        SChapter.create().apply {
             name = element.text()
             url = element.attr("href")
         }
-    }
 
-    /* ===== Pages ===== */
-    override fun pageListParse(document: Document): List<Page> {
-        return document.select("img").mapIndexed { i, img ->
+    override fun pageListParse(document: Document): List<Page> =
+        document.select("img").mapIndexed { i, img ->
             Page(i, "", img.attr("src"))
         }
-    }
 
     override fun imageUrlParse(document: Document) = ""
 }
