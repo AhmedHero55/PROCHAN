cd3536830b9bfba7a1c666eca0414592e71e87cb
AhmedHero55 <sepertiten@gmail.com>
2025-12-19 17:01:28 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,5 +6,13 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "PROCHAN"
-include(":app")   // ✅ تعريف الوحدة الفرعية app
+include(":app")
