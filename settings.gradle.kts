70477f66123fda4786b913be45d37b37c0ca0cb5
AhmedHero55 <sepertiten@gmail.com>
2025-12-19 16:39:58 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,7 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
-    plugins {
-        id("com.android.application") version "8.5.0"
-        id("org.jetbrains.kotlin.android") version "2.0.21"
-    }
 }
+
 rootProject.name = "PROCHAN"
-include(":app")
+include(":app")   // ✅ تعريف الوحدة الفرعية app
