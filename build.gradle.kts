8dac621ea977a136f5f5d19caa6d7a85c961a505
AhmedHero55 <sepertiten@gmail.com>
2025-12-19 22:33:05 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("com.android.application") version "8.5.0" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    // تحديث Kotlin ليتوافق مع AAR المبني على 2.3.0
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
 }
