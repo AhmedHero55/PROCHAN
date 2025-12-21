dbe309ea6a4f393f627bedb2ce7c76026626cbdc
AhmedHero55 <sepertiten@gmail.com>
2025-12-21 21:40:42 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,3 @@
-// ملف الجذر build.gradle.kts
-
 plugins {
     id("com.android.application") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0" apply false
@@ -8,3 +6,22 @@ plugins {
 task<Delete>("clean") {
     delete(rootProject.buildDir)
 }
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "PROCHAN"
+include(":app")
